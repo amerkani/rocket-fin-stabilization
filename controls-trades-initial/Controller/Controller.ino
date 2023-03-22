f2109dc1a809fb8f46e666365bbab0dde0206107
NSuttles1 <124738585+NSuttles1@users.noreply.github.com>
2023-03-22 15:13:50 -0400

    Add files via upload

--- controls-trades-initial/Controller/Controller.ino
+++ controls-trades-initial/Controller/Controller.ino
@@ -1,68 +1,38 @@
-/*
-  SD card datalogger
-
-  The circuit:
-   analog sensors on analog ins 0, 1, and 2
-   SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 10
-*/
-
-#include <SPI.h>
-#include <SD.h>
 #include <Wire.h>
-#include <RH_ASK.h>
 #include <Servo.h>
 const int MPU = 0x68; // MPU6050 I2C address
 float ASens, GSens;
 float AccX, AccY, AccZ;
-float GyroX, GyroY, GyroZ;
+float GyroX, GyroY, GyroZ, GyroOld, GyroSum;
 float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
-float Uin, Up;
-float elapsedTime, currentTime, previousTime;
+float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
+float elapsedTime;
+long  previousTime;
+float K1, K2;
+float theta,u;
 int c = 0;
-int Samples = 0;
-int Transmitter = 0;
-int RandT, start;
 
-File dataFile;
-
-const int chipSelect = 10;
-
-RH_ASK driver;
-Servo servo;
+Servo servo1;
+Servo servo2;
+Servo servo3;
 
 void setup() {
-
-  servo.attach(10);
+  K1 = 1.000;
+  K2 = 1.597;
+    
+  servo1.attach(10);
+  servo2.attach(11);
+  servo3.attach(12);
 
   // Open serial communications and wait for port to open:
   Serial.begin(19200);
-  SPI.begin();
   Wire.begin();                      // Initialize comunication
+  Wire.setWireTimeout(3000 /* us */, true /* reset_on_timeout */);
   Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
   Wire.write(0x6B);                  // Talk to the register 6B
   Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
   Wire.endTransmission(true);        //end the transmission
- 
-  if (!driver.init())
-  Serial.println("Receiver init failed");
-
-  Serial.print("Initializing SD card...");
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1);
-  }
-  Serial.println("card initialized.");
-
-  dataFile = SD.open("test.txt", FILE_WRITE);
-  dataFile.close();
-
+  
   // Configure Accelerometer Sensitivity - Full Scale Range (default +/- 2g)
   Wire.beginTransmission(MPU);
   Wire.write(0x1C);                  //Talk to the ACCEL_CONFIG register (1C hex)
@@ -75,88 +45,14 @@ void setup() {
   Wire.endTransmission(true);
   ASens = 8192.0;
   GSens = 65.5;
-  
+
   // Call this function if you need to get the IMU error values for your module
   calculate_IMU_error();
-
-  uint8_t buf[1];
-  uint8_t buflen = sizeof(buf);
-while(Transmitter == 0){
-Transmitter = driver.recv(buf, &buflen);
+  delay(5);
+  previousTime = millis();
 }
-}
-
 
 void loop() {
-  switch (Transmitter) {
-
-    case 1:
-    analogWrite(10,1);    
-    Up = 5;
-    servo.write(Up+90);
-    break;
-
-    case 10:
-    analogWrite(10,1);    
-    Up = 5;
-    servo.write(Up+90);
-    break;
-
-    case 2:
-    analogWrite(10,1);
-    Up = (180/PI)*sin(millis()/1000);
-    servo.write(Up+90);
-    break;
-
-    case 20:
-    Up = (180/PI)*sin(millis()/1000);
-    servo.write(Up+90);
-    break;
-
-    case 3:
-    analogWrite(10,1);
-    if(Up == 5 & (millis()-start)>RandT){
-      Up = 0 ;
-      servo.write(Up+90);
-      RandT = random(2000,4000);
-      start = millis();
-    }
-    if(Up == 0 & (millis()-start)>RandT){
-      Up = 5 ;
-      servo.write(Up+90);
-      RandT = random(2000,4000);
-      start = millis();
-    }
-    break;
-
-    case 30:
-    if(Up == 5 & (millis()-start)>RandT){
-      Up = 0 ;
-      servo.write(Up+90);
-      RandT = random(2000,4000);
-      start = millis();
-    }
-    if(Up == 0 & (millis()-start)>RandT){
-      Up = 5 ;
-      servo.write(Up+90);
-      RandT = random(2000,4000);
-      start = millis();
-    }
-    break;
-
-    case 4:
-    analogWrite(10,1);
-    Up = 0;
-    servo.write(Up+90);
-    break;
-
-    default:
-    Up = 0;
-    servo.write(Up+90);
-    break;
-  }
-
-
   // === Read acceleromter data === //
   Wire.beginTransmission(MPU);
   Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
@@ -165,9 +61,16 @@ void loop() {
   //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
   AccX = (Wire.read() << 8 | Wire.read()) / ASens; // X-axis value
   AccY = (Wire.read() << 8 | Wire.read()) / ASens; // Y-axis value
-  AccZ = (Wire.read() << 8 | Wire.read()) / ASens; // Z-axis value
+  AccZ = (Wire.read() << 8 | Wire.read()) / ASens; // Z-axis value    
 
+  // Calculating Roll and Pitch from the accelerometer data
+  accAngleX = (atan2(AccY , sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorX; // AccErrorX ~(1.55) See the calculate_IMU_error()custom function for more details
+  accAngleY = (atan2(AccX , sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorY; // AccErrorY ~(-1.14)
   // === Read gyroscope data === //
+
+  elapsedTime = (millis() - previousTime) / 1000; // Divide by 1000 to get seconds
+  previousTime = millis();
+
   Wire.beginTransmission(MPU);
   Wire.write(0x43); // Gyro data first register address 0x43
   Wire.endTransmission(false);
@@ -175,45 +78,45 @@ void loop() {
   GyroX = (Wire.read() << 8 | Wire.read()) / GSens; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
   GyroY = (Wire.read() << 8 | Wire.read()) / GSens;
   GyroZ = (Wire.read() << 8 | Wire.read()) / GSens;
+
   // Correct the outputs with the calculated error values
-  GyroX = GyroX - GyroErrorX; // GyroErrorX ~(-0.29)
-  GyroY = GyroY - GyroErrorY; // GyroErrorY ~(0.35)
-  GyroZ = GyroZ - GyroErrorZ; // GyroErrorZ ~ (-0.13)
+  GyroX = GyroX - GyroErrorX; // GyroErrorX ~(-5.92)
+  GyroY = GyroY - GyroErrorY; // GyroErrorY ~(1.05)
+  GyroZ = GyroZ - GyroErrorZ; // GyroErrorZ ~ (-1.17)
 
-  // Read servo inputs
-  Uin = analogRead(A0);
+  // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
+  gyroAngleY = gyroAngleY + GyroY * elapsedTime;// deg/s * s = deg
 
-  // append sensor data to the string:
-    // make a string for assembling the data to log:
-  String dataString = String(AccX) + "," + String(AccY) + "," + String(AccZ) + "," + String(GyroX) + "," + String(GyroY) + "," + String(GyroZ) + "," + String(Uin) + "," + String(Up) + "," + String(millis());
+  // Complementary filter - combine acceleromter and gyro angle values
+  theta = 0.96 * gyroAngleY + 0.04 * accAngleY;
+
+ // === Calculate desired servo value ===//
+  u = -K1*theta-K2*(GyroY);
+
+  analogWrite(44,u);
   
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  dataFile = SD.open("test.txt", FILE_WRITE);
-  // if the file is available, write to it:
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
+  if (abs(u) > 15) {
+    u = (abs(u)/u)*15;
   }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening test.txt");
-    return;
-  }
+
+  servo1.write(u+90);
+  servo2.write(u+90);
+  servo3.write(u+90);
+
 }
 
 void calculate_IMU_error() {
   // We can call this funtion in the setup section to calculate the accelerometer and gyro data error. From here we will get the error values used in the above equations printed on the Serial Monitor.
   // Note that we should place the IMU flat in order to get the proper values, so that we then can the correct values
-  // Read accelerometer values 200 times
+  // Read accelerometer values 300 times
   while (c < 300) {
     Wire.beginTransmission(MPU);
     Wire.write(0x3B);
     Wire.endTransmission(false);
     Wire.requestFrom(MPU, 6, true);
-    AccX = (Wire.read() << 8 | Wire.read()) / ASens; // X-axis value
-    AccY = (Wire.read() << 8 | Wire.read()) / ASens; // Y-axis value
-    AccZ = (Wire.read() << 8 | Wire.read()) / ASens; // Z-axis value    
+    AccX = (Wire.read() << 8 | Wire.read()) / ASens ;
+    AccY = (Wire.read() << 8 | Wire.read()) / ASens ;
+    AccZ = (Wire.read() << 8 | Wire.read()) / ASens ;
     // Sum all readings
     AccErrorX = AccErrorX + ((atan2(AccY , sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / PI));
     AccErrorY = AccErrorY + ((atan2(AccX , sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / PI));
@@ -243,21 +146,14 @@ void calculate_IMU_error() {
   GyroErrorY = GyroErrorY / c;
   GyroErrorZ = GyroErrorZ / c;
   // Print the error values on the Serial Monitor
-  Serial.print("AccErrorX: ");
+  Serial.print(F("AccErrorX: "));
   Serial.println(AccErrorX);
-  Serial.print("AccErrorY: ");
+  Serial.print(F("AccErrorY: "));
   Serial.println(AccErrorY);
-  Serial.print("GyroErrorX: ");
+  Serial.print(F("GyroErrorX: "));
   Serial.println(GyroErrorX);
-  Serial.print("GyroErrorY: ");
+  Serial.print(F("GyroErrorY: "));
   Serial.println(GyroErrorY);
-  Serial.print("GyroErrorZ: ");
+  Serial.print(F("GyroErrorZ: "));
   Serial.println(GyroErrorZ);
 }
-
-
-
-
-
-
-
